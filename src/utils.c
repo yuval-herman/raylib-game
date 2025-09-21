6824252f2067e6f2c8d6af1f6915eb1c62c34c42
Yuval Herman <yuvalherman99@gmail.com>
2025-09-21 20:10:35 +0300

    to fresh beginnings 🥂

--- src/utils.c
+++ src/utils.c
@@ -1,8 +1,15 @@
+#include "utils.h"
 #ifdef _WIN32
 #include <windows.h>
 #else
 #include <unistd.h>
 #endif
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
 
 int n_cores(void)
 {
@@ -17,4 +24,36 @@ int n_cores(void)
 #else
     return 1;
 #endif
+}
+
+void log_msg(LOG_LEVEL level, const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+
+    char buffer[256] = {0};
+
+    switch (level)
+    {
+    case LOG_DEBUG:
+        strcpy(buffer, "DEBUG: ");
+        break;
+    case LOG_INFO:
+        strcpy(buffer, "INFO: ");
+        break;
+    case LOG_WARN:
+        strcpy(buffer, "WARNING: ");
+        break;
+    case LOG_ERROR:
+        strcpy(buffer, "ERROR: ");
+        break;
+    default:
+        break;
+    }
+
+    unsigned int textSize = (unsigned int)strlen(fmt);
+    memcpy(buffer + strlen(buffer), fmt, (textSize < (256 - 12)) ? textSize : (256 - 12));
+    strcat(buffer, "\n");
+    vfprintf(stderr, buffer, args);
+    va_end(args);
 }
--- src/utils.h
+++ src/utils.h
@@ -1,20 +1,17 @@
 #ifndef _UTILS_H
 #define _UTILS_H
 
-// Number of CPU cores on the machine
-int n_cores(void);
-void TraceLog(int logLevel, const char *text, ...); // Show trace log messages (LOG_DEBUG, LOG_INFO, LOG_WARNING, LOG_ERROR...)
-
 typedef enum
 {
-    LOG_ALL = 0, // Display all logs
-    LOG_TRACE,   // Trace logging, intended for internal use only
-    LOG_DEBUG,   // Debug logging, used for internal debugging, it should be disabled on release builds
-    LOG_INFO,    // Info logging, used for program execution info
-    LOG_WARNING, // Warning logging, used on recoverable failures
-    LOG_ERROR,   // Error logging, used on unrecoverable failures
-    LOG_FATAL,   // Fatal logging, used to abort program: exit(EXIT_FAILURE)
-    LOG_NONE     // Disable logging
-} TraceLogLevel;
+    LOG_DEBUG,
+    LOG_INFO,
+    LOG_WARN,
+    LOG_ERROR
+} LOG_LEVEL;
+
+// Number of CPU cores on the machine
+int n_cores(void);
+/* Log to stderr with timestamp, level, and printf-style formatting */
+void log_msg(LOG_LEVEL level, const char *fmt, ...);
 
 #endif // _UTILS_H
