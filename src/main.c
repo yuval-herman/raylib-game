6bc9eb8f2f15f6c9c5fcbf74b99a5e0f5113d2a4
Yuval Herman <yuvalherman99@gmail.com>
2026-03-11 20:52:48 +0200

    Improved simple starter project

--- src/main.c
+++ src/main.c
@@ -1,23 +1,16 @@
-#include <stdio.h>
 #include "raylib.h"
 
-#define SCREEN_HEIGHT 1440
-#define SCREEN_WIDTH 2560
-
-int main(void)
-{
-  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "our game");
-  while (true)
-  {
+int main(void) {
+  SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_WINDOW_RESIZABLE);
+  InitWindow(800, 600, "our game");
+  const char *text = "For new beginnings!";
+  const Vector2 text_measurements =
+      MeasureTextEx(GetFontDefault(), text, 30, 1);
+  while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(RAYWHITE);
-    DrawText("For new beginnings!", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 72, BLACK);
+    DrawText(text, (GetScreenWidth() - text_measurements.x) / 2, (GetScreenHeight() - text_measurements.y) / 2, 30, BLACK);
     EndDrawing();
-
-    if (IsKeyPressed(KEY_Q))
-    {
-      return 0;
-    }
   }
   return 0;
 }
