af1e13d86e9314d79db743b302aa30f5b376450b
Yuval Herman <yuvalherman99@gmail.com>
2026-03-11 21:10:04 +0200

    basic movement

--- src/main.c
+++ src/main.c
@@ -1,15 +1,56 @@
 #include "raylib.h"
 
+const int GROUND_LEVEL = 0;
+const float GRAVITY_LEVEL = 1;
+
+Camera2D camera = {.zoom = 1};
+Rectangle player_rect = {.height = 20, .width = 20, .y = 2};
+Vector2 player_velocity = {0};
+
+void update_camera() {
+  camera.offset.x = GetScreenWidth() / 2.0;
+  camera.offset.y = GetScreenHeight() / 2.0;
+  camera.target.x = player_rect.x;
+  camera.target.y = player_rect.y;
+}
+
+void update_player() {
+  if (player_rect.y >= GROUND_LEVEL - player_rect.height) {
+    player_rect.y = GROUND_LEVEL - player_rect.height;
+    player_velocity.y = 0;
+    if (IsKeyDown(KEY_SPACE)) {
+      player_velocity.y -= 10;
+    }
+  } else {
+    player_velocity.y += GRAVITY_LEVEL;
+  }
+  if (IsKeyDown(KEY_LEFT)) {
+    player_velocity.x -= 10;
+  } else if (IsKeyDown(KEY_RIGHT)) {
+    player_velocity.x += 10;
+  }
+  player_velocity.x /= 2;
+  player_rect.x += player_velocity.x;
+  player_rect.y += player_velocity.y;
+}
+
 int main(void) {
   SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_WINDOW_RESIZABLE);
   InitWindow(800, 600, "our game");
-  const char *text = "For new beginnings!";
-  const Vector2 text_measurements =
-      MeasureTextEx(GetFontDefault(), text, 30, 1);
+
+  SetTargetFPS(60);
+
   while (!WindowShouldClose()) {
+    update_camera();
+    update_player();
+
     BeginDrawing();
+    BeginMode2D(camera);
     ClearBackground(RAYWHITE);
-    DrawText(text, (GetScreenWidth() - text_measurements.x) / 2, (GetScreenHeight() - text_measurements.y) / 2, 30, BLACK);
+
+    DrawRectangleRec(player_rect, RED);
+    DrawRectangle(0, 0, 200, 10, BLACK);
+
     EndDrawing();
   }
   return 0;
