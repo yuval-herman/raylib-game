78284997c93a3b00453c70363575665091fb2cc2
itamaroren8 <itamarorenstei@gmail.com>
2026-03-11 22:26:10 +0200

    Added frametime to support all fps brackets

--- src/main.c
+++ src/main.c
@@ -1,7 +1,13 @@
 #include "raylib.h"
 
 const int GROUND_LEVEL = 0;
-const float GRAVITY_LEVEL = 1;
+const float GRAVITY_LEVEL = 1000;
+const int JUMP_POWER = 400;
+const int MOVEMENT_SPEED = 400;
+const int FPS = 60;
+
+bool canDoubleJump;
+bool isGrounded;
 
 Camera2D camera = {.zoom = 1};
 Rectangle player_rect = {.height = 20, .width = 20, .y = 2};
@@ -15,30 +21,47 @@ void update_camera() {
 }
 
 void update_player() {
-  if (player_rect.y >= GROUND_LEVEL - player_rect.height) {
+  if (player_rect.y >= GROUND_LEVEL - player_rect.height) { // Is grounded
     player_rect.y = GROUND_LEVEL - player_rect.height;
     player_velocity.y = 0;
-    if (IsKeyDown(KEY_SPACE)) {
-      player_velocity.y -= 10;
+    isGrounded = true;
+    canDoubleJump = true;
+  } else { // Isn't grounded
+    player_velocity.y += GRAVITY_LEVEL * GetFrameTime();
+    isGrounded = false;
+  }
+
+
+  if (IsKeyPressed(KEY_SPACE)) {
+    if (isGrounded)
+        player_velocity.y = -JUMP_POWER;
+    else if (canDoubleJump) {
+        player_velocity.y = -JUMP_POWER;
+        canDoubleJump = false;
     }
-  } else {
-    player_velocity.y += GRAVITY_LEVEL;
   }
+
+  if (IsKeyReleased(KEY_SPACE) && player_velocity.y < 0)
+  {
+    player_velocity.y *= 0.5;
+  }
+
   if (IsKeyDown(KEY_LEFT)) {
-    player_velocity.x -= 10;
+    player_velocity.x -= MOVEMENT_SPEED;
   } else if (IsKeyDown(KEY_RIGHT)) {
-    player_velocity.x += 10;
+    player_velocity.x += MOVEMENT_SPEED;
   }
+
   player_velocity.x /= 2;
-  player_rect.x += player_velocity.x;
-  player_rect.y += player_velocity.y;
+  player_rect.x += player_velocity.x * GetFrameTime();
+  player_rect.y += player_velocity.y * GetFrameTime();
 }
 
 int main(void) {
   SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_WINDOW_RESIZABLE);
   InitWindow(800, 600, "our game");
 
-  SetTargetFPS(60);
+  SetTargetFPS(FPS);
 
   while (!WindowShouldClose()) {
     update_camera();
@@ -50,6 +73,7 @@ int main(void) {
 
     DrawRectangleRec(player_rect, RED);
     DrawRectangle(0, 0, 200, 10, BLACK);
+    DrawText(TextFormat("Speed: %f", player_velocity.x), 15, 15, 30, BLACK);
 
     EndDrawing();
   }
