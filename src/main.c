f20ffd77497762e1dd60aee47dc6edb5dd71bcde
itamaroren8 <itamarorenstei@gmail.com>
2026-03-11 20:40:13 +0200

    Added raylib stuff

--- src/main.c
+++ src/main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include "raylib.h"
 
-int main(void) {
-  printf("For new begginings!\n");
+#define SCREEN_HEIGHT 1440
+#define SCREEN_WIDTH 2560
+
+int main(void)
+{
+  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "our game");
+  while (true)
+  {
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    DrawText("For new beginnings!", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 72, BLACK);
+    EndDrawing();
+
+    if (IsKeyPressed(KEY_Q))
+    {
+      return 0;
+    }
+  }
   return 0;
 }
